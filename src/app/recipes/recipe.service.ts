201b74451b3f5bbca0ab9216c60c7a96d2e6d1f9
Jan Hazincak <jan.hazincak@gmail.com>
2020-11-08 00:14:02 +0000

    Adding ingredients to recpipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,31 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model'
 
 export class RecipeService{
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A test recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-        new Recipe('A test recipe2', 'This is simply a test2', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+        new Recipe(
+            'Tasty Schnitzel',
+            'A super-tasty Schnitzel - just awesome!',
+            'https://upload.wikimedia.org/wikipedia/commons/9/92/Wiener_Schnitzel_in_Wachwitz_%281%29.JPG',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]
+            ),
+        new Recipe(
+            'Big Fat Burger',
+            'This is simply a test2',
+            'https://upload.wikimedia.org/wikipedia/commons/7/7f/Hamburger_%282%29.jpg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Salad', 1),
+                new Ingredient('Meat', 1),
+                new Ingredient('Garlic Dip', 1)
+            ]
+            ),
       ];
 
     getRecipes(){
