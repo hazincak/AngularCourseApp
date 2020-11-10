32ffc82a478a7fcd8d527ccae9e17a1a76d02038
Jan Hazincak <jan.hazincak@gmail.com>
2020-11-10 14:00:27 +0000

    Setting Up Routes

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,7 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
+Injectable()
 export class ShoppingListService{
     ingredientsChanged = new EventEmitter<Ingredient[]>();
     private ingredients: Ingredient[]= [
