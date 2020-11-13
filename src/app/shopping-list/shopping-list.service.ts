7f9019843738ececa89ddba066a31e349b4c95e7
Jan Hazincak <jan.hazincak@gmail.com>
2020-11-13 04:45:17 +0000

    Updating existing items

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -36,5 +36,10 @@ export class ShoppingListService{
         this.ingredientsChanged.next(this.ingredients.slice());
       }
 
+      updateIngredient(index: number, newIngredient: Ingredient){
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next(this.ingredients.slice());
+      }
+
 
 }
