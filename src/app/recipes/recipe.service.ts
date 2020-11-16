aea5796fa35b5a6dc51411c9f11bbe6edec7ba1c
Jan Hazincak <jan.hazincak@gmail.com>
2020-11-16 06:44:21 +0000

    Transofming Response Data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,29 +7,32 @@ import { Recipe } from './recipe.model'
 @Injectable()
 export class RecipeService{
     recipesChanged = new Subject<Recipe[]>();
+
+    private recipes: Recipe[];
    
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Tasty Schnitzel',
-            'A super-tasty Schnitzel - just awesome!',
-            'https://upload.wikimedia.org/wikipedia/commons/9/92/Wiener_Schnitzel_in_Wachwitz_%281%29.JPG',
-            [
-                new Ingredient('Meat', 1),
-                new Ingredient('French Fries', 20)
-            ]
-            ),
-        new Recipe(
-            'Big Fat Burger',
-            'This is simply a test2',
-            'https://upload.wikimedia.org/wikipedia/commons/7/7f/Hamburger_%282%29.jpg',
-            [
-                new Ingredient('Buns', 2),
-                new Ingredient('Salad', 1),
-                new Ingredient('Meat', 1),
-                new Ingredient('Garlic Dip', 1)
-            ]
-            ),
-      ];
+    // private recipes: Recipe[] 
+    // = [
+    //     new Recipe(
+    //         'Tasty Schnitzel',
+    //         'A super-tasty Schnitzel - just awesome!',
+    //         'https://upload.wikimedia.org/wikipedia/commons/9/92/Wiener_Schnitzel_in_Wachwitz_%281%29.JPG',
+    //         [
+    //             new Ingredient('Meat', 1),
+    //             new Ingredient('French Fries', 20)
+    //         ]
+    //         ),
+    //     new Recipe(
+    //         'Big Fat Burger',
+    //         'This is simply a test2',
+    //         'https://upload.wikimedia.org/wikipedia/commons/7/7f/Hamburger_%282%29.jpg',
+    //         [
+    //             new Ingredient('Buns', 2),
+    //             new Ingredient('Salad', 1),
+    //             new Ingredient('Meat', 1),
+    //             new Ingredient('Garlic Dip', 1)
+    //         ]
+    //         ),
+    //   ];
 
     constructor(private shoppingListService: ShoppingListService){}
 
