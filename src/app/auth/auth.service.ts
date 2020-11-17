868249d4ef8714894ce8dbc428194f6595a217fe
Jan Hazincak <jan.hazincak@gmail.com>
2020-11-17 03:20:18 +0000

    Preparing the Signup Request

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+interface AuthResponseData{
+    kind: string;
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    expiresIn: string;
+    localId: string
+}
+
+@Injectable({providedIn: 'root'})
+export class AuthService{
+
+    constructor(private http: HttpClient){}
+
+    signup(email: string, password: string){
+        return this.http.post<AuthResponseData>(
+            `https://identitytoolkit.googleapis.com
+            /v1/accounts:signInWithCustomToken?key=A
+            IzaSyBMIiQDqqNxXMfHwy780VstgnWg7-ybAXs`, 
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+    
+            })
+        
+    }
+}
