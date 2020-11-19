1e2e5ff73ab1cfd241f2de0190e529bf325f45df
Jan Hazincak <jan.hazincak@gmail.com>
2020-11-19 03:39:37 +0000

    Adding Auto-Logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -21,7 +21,7 @@ export interface AuthResponseData{
 export class AuthService{
 
     user = new BehaviorSubject<User>(null);
-    
+    private tokenExpirationTimer: any;
 
     constructor(private http: HttpClient,
                 private router: Router){}
@@ -46,7 +46,7 @@ export class AuthService{
         const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
             const user = new User (email, userId, token, expirationDate);
             this.user.next(user);
-
+            this.autoLogout(expiresIn * 1000);
             localStorage.setItem('userData', JSON.stringify(user));
     }
 
@@ -65,6 +65,9 @@ export class AuthService{
     
         if(loadedUser.token){
             this.user.next(loadedUser);
+            const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime()
+
+            this.autoLogout(expirationDuration)
         }
     }
 
@@ -103,6 +106,18 @@ export class AuthService{
     logout(){
         this.user.next(null);
         this.router.navigate(['/auth']);
+        localStorage.removeItem('userData');
+        if(this.tokenExpirationTimer){
+            clearTimeout(this.tokenExpirationTimer);
+        }
+        this.tokenExpirationTimer = null;
     }
+
+    autoLogout(expirationDuration: number){
+       this.tokenExpirationTimer = setTimeout(() => {
+            this.logout();
+        }, expirationDuration);
+    }
+
 
 }
