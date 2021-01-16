8a51537646f069a2b4b9106e26eacff86655e41a
Jan Hazincak <jan.hazincak@gmail.com>
2021-01-16 20:11:26 +0000

    Understanding shared modules

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
-import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
 
 import { RecipesComponent } from "./recipes.component";
@@ -10,6 +9,7 @@ import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.compo
 import { RecipeListComponent } from "./recipe-list/recipe-list.component";
 import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
 import { RecipesRoutingModule } from "./recipes-routing.module";
+import { SharedModule } from "../shared/shared.module";
 
 
 
@@ -24,9 +24,9 @@ import { RecipesRoutingModule } from "./recipes-routing.module";
   ],
   imports: [
     RouterModule,
-    CommonModule,
     ReactiveFormsModule,
-    RecipesRoutingModule
+    RecipesRoutingModule,
+    SharedModule
   ],
 })
 export class RecipesModule {}
