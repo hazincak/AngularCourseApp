6fd682a5c8275d200be2c0e0e66a25ec46675de8
Jan Hazincak <jan.hazincak@gmail.com>
2021-01-19 23:03:15 +0000

    Implementing lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes=[
     { path: '', redirectTo: '/recipes', pathMatch: 'full'},
+
+    //old syntax for lazy loading
+    // { path: 'recipes', loadChildren: './recipes/recipes.module.ts#RecipesModule' }
+    //loadChildren is a special property in route config which only load code content when anyone visits /recipes
+
+    //new syntax for lazy loading
+    { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule) }
 ];
 
 @NgModule({
