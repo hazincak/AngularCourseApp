024855baca4ff41b4447c92a8de15869bafb3426
Jan Hazincak <jan.hazincak@gmail.com>
2021-01-19 23:10:28 +0000

    More Lazy Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,10 @@ const appRoutes: Routes=[
     //loadChildren is a special property in route config which only load code content when anyone visits /recipes
 
     //new syntax for lazy loading
-    { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule) }
+    { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule) },
+    { path: 'shopping-list', loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule) },
+    { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent} from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
+
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
 
 
 
@@ -21,8 +20,6 @@ import { AuthModule } from './auth/auth.module';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ShoppingListModule,
-    AuthModule,
     SharedModule,
     CoreModule
   ],
