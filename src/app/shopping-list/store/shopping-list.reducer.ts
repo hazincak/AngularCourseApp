96355ef1428c4ce1bbf650959438f09f35999144
Jan Hazincak <jan.hazincak@gmail.com>
2021-01-21 00:51:26 +0000

    Setting Up the NgRx Store

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,6 +1,7 @@
-import { Action } from "@ngrx/store";
+
 import { Ingredient } from "../../shared/ingredient.model";
-import { ADD_INGREDIENT } from './shopping-list.actions';
+// import { ADD_INGREDIENT } from './shopping-list.actions';
+import * as ShoppingListActions from './shopping-list.actions';
 
 const initialState = {
   ingredients: [
@@ -10,12 +11,12 @@ const initialState = {
 };
 
 //state = initialState asigns default values to function arguments. IF the argument no set.
-export function shoppingListReducer(state = initialState, action: Action){
+export function shoppingListReducer(state = initialState, action: ShoppingListActions.AddIngredient){
   switch (action.type){
-    case ADD_INGREDIENT:
+    case ShoppingListActions.ADD_INGREDIENT:
       return {
         ...state,
-        ingredients: [...state.ingredients, action]
+        ingredients: [...state.ingredients, action.payload]
       }
   }
 }
