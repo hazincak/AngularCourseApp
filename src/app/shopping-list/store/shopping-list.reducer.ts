e06b039dc20e557e06dd4360f482cf48f450c880
Jan Hazincak <jan.hazincak@gmail.com>
2021-01-24 09:01:10 +0000

    Updating & Deleting Ingredients

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -23,7 +23,28 @@ export function shoppingListReducer(state = initialState, action: ShoppingListAc
         ...state,
         ingredients: [...state.ingredients, ...action.payload]
       }
-      default:
-        return state;
+    case ShoppingListActions.UPDATE_INGREDIENT:
+      const ingredient = state.ingredients[action.payload.index];
+      const updatedIngredient = {
+        ...ingredient,
+        ...action.payload.ingredient
+      };
+      const updatedIngredients = [...state.ingredients];
+      updatedIngredients[action.payload.index] = updatedIngredient;
+
+      return{
+        ...state,
+        ingredients: updatedIngredients
+      }
+    case ShoppingListActions.DELETE_INGREDIENT:
+      return{
+        ...state,
+        ingredients: state.ingredients.filter( (ingredient, ingredientIndex ) => {
+          return ingredientIndex !== action.payload;
+        })
+      }
+
+    default:
+      return state;
   }
 }
