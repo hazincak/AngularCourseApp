3e7947d8eff7a1ac319282bf7ba8b0135f2da6d9
Jan Hazincak <jan.hazincak@gmail.com>
2021-03-06 02:17:51 +0000

    Installation Redux devtools )_--save-dev @ngrx/store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent} from './header/header.component';
@@ -13,6 +14,7 @@ import { CoreModule } from './core.module';
 import { LoggingService } from './logging.service';
 import * as fromApp from './store/app.reducer';
 import { AuthEffects } from './auth/store/auth.effects';
+import { environment } from 'src/environments/environment';
 
 
 
@@ -27,6 +29,7 @@ import { AuthEffects } from './auth/store/auth.effects';
     HttpClientModule,
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({logOnly: environment.production}),
     SharedModule,
     CoreModule
   ],
