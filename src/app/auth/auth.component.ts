3413ff4fa6c3b04eda7f3d23dd210b7672b7a0e9
Jan Hazincak <jan.hazincak@gmail.com>
2021-03-06 02:07:20 +0000

    Finishing the Auth Effects

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,6 @@
 import { Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
-import { Observable, Subscription } from 'rxjs';
-import { AuthResponseData, AuthService } from './auth.service';
+import { Subscription } from 'rxjs';
 import { AlertComponent } from '../shared/alert/alert.component'
 import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
 import { Store } from '@ngrx/store';
@@ -22,8 +20,7 @@ export class AuthComponent implements OnInit, OnDestroy{
     private closeSubscription: Subscription
     private storeSub: Subscription
 
-    constructor(private authService: AuthService,
-                private router: Router,
+    constructor(
                 private componentFactoryResolver: ComponentFactoryResolver,
                 private store: Store<fromApp.AppState>){}
 
